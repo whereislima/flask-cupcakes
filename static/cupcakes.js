2ecb96d939f4478609cf15d66a4c6ac8be01b5af
whereislima <reach.lima@gmail.com>
2021-05-18 18:14:19 -0700

    completed frontend in js

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -3,24 +3,26 @@ const BASE_URL = "http://localhost:5000/api";
 
 function generateCupcakeHTML(cupcake) {
   return `
+  <div data-cupcake-id=${cupcake.id}>
     <li> ${cupcake.flavor} / ${cupcake.size} / ${cupcake.rating}
-    <button>delete cupcake</button>
+    <button class="delete-cupcake">X</button>
     </li>
     <img class="cupcake-image" src="${cupcake.image}" width="300" height="400">
-    `
+    </div>
+    `;
 }
 
 async function showInitialCupcakes() {
   const response = await axios.get(`${BASE_URL}/cupcakes`);
 
-  for(let cupcakeData of response.data.cupcakes) {
+  for (let cupcakeData of response.data.cupcakes) {
     let newCupcake = $(generateCupcakeHTML(cupcakeData));
     $("#cupcakes-list").append(newCupcake);
   }
 }
 
 // handle form submit for new cupcake and add to api
-$("#new-cupcake-form").on("submit", async function(evt) {
+$("#new-cupcake-form").on("submit", async function (evt) {
   evt.preventDefault();
 
   // get form values
@@ -33,7 +35,7 @@ $("#new-cupcake-form").on("submit", async function(evt) {
   const newCupcakeResponse = await axios.post(`${BASE_URL}/cupcakes`, {
     flavor,
     rating,
-    size, 
+    size,
     image
   });
 
@@ -42,7 +44,14 @@ $("#new-cupcake-form").on("submit", async function(evt) {
   $("#new-cupcake-form").trigger("reset");
 })
 
+$("#cupcakes-list").on("click", ".delete-button", async function (evt) {
+  evt.preventDefault();
 
+  let $cupcake = $(evt.target).closest("div");
+  let cupcakeId = $cupcake.attr("data-cupcake-id");
+  
+  await axios.delete(`${BASE_URL}/cupcakes/${cupcakeId}`);
+  $cupcake.remove();
+})
 
 $(showInitialCupcakes);
-
